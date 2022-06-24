9cacb32311124d297ac323c39143b0c6d4642313
Erum-Rehman <erumr0443@gmail.com>
2022-06-24 20:27:54 +0500

    Initialize project using Create React App

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import './index.scss';
+
+const Footer = () => {
+    return(
+        <>
+        <div className='main_footer'>
+            <h1>GET OUR LATEST UPDATES AND NEWS FIRST</h1>
+        </div>
+        </>
+    )
+}
+export default Footer;
